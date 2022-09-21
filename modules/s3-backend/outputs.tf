57600c1f1348479d7606c48b235ed04a659e3e11
Cwagne17 <cwagne17@students.towson.edu>
2022-09-21 09:57:37 -0400

    added output and input variables

--- modules/s3-backend/outputs.tf
+++ modules/s3-backend/outputs.tf
@@ -1,29 +1,29 @@
 output "bucket_name" {
   description = "Name of S3 bucket"
-  value = aws_s3_bucket.backend.bucket
+  value       = aws_s3_bucket.backend.bucket
 }
 
 output "bucket_region" {
   description = "AWS region S3 bucket is in"
-  value = aws_s3_bucket.backend.region
+  value       = aws_s3_bucket.backend.region
 }
 
 output "s3_backend_policy_arn" {
   description = "ARN of IAM policy"
-  value = aws_iam_policy.tf_s3_backend_policy.arn
+  value       = aws_iam_policy.tf_s3_backend_policy.arn
 }
 
 output "s3_backend_policy_name" {
   description = "Name of IAM policy"
-  value = aws_iam_policy.tf_s3_backend_policy.name
+  value       = aws_iam_policy.tf_s3_backend_policy.name
 }
 
 output "s3_backend_role_arn" {
   description = "ARN of IAM role"
-  value = module.iam_assumable_role.iam_role_arn
+  value       = module.iam_assumable_role.iam_role_arn
 }
 
 output "s3_backend_role_name" {
   description = "Name of IAM role"
-  value = module.iam_assumable_role.iam_role_name
+  value       = module.iam_assumable_role.iam_role_name
 }
