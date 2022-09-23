6a061a5c2d2d0ce7c4fa1908be66eb4f5035d470
Andy Stoerkel <astoer1@students.towson.edu>
2022-09-23 09:48:09 -0400

    added autoscaling module

--- modules/ecs-cluster/variables.tf
+++ modules/ecs-cluster/variables.tf
@@ -6,40 +6,6 @@ variable "project_name" {
   description = "name that will be appended to all default names"
 }
 # asg
-variable "launch_template_ami" {
-  type        = string
-  description = "the ami image number for the ec2 instance to be launched"
-}
-
-variable "instance_type" {
-  type        = string
-  description = "the type of instance to launch (e.g. t2.micro)"
-}
-
-variable "public_subnets" {
-  type        = list(string)
-  description = "the list of public subnets from the vpc"
-}
-
-variable "private_subnets" {
-  type        = list(string)
-  description = "the list of private subnets from the VPC"
-}
-
-variable "asg_max_size" {
-  type        = number
-  description = "maximum size of the autoscaling group"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "VPC id to create the cluster in"
-}
-
-variable "security_group_ids" {
-  type        = list(string)
-  description = "list of security group ids to associate with the autoscaling group"
-}
 
 variable "autoscaling_capacity_providers" {
   type = map(object({
@@ -53,31 +19,4 @@ variable "autoscaling_capacity_providers" {
       target_capacity           = number
     }
   }))
-}
-########################################
-# Optional vars
-########################################
-
-variable "asg_min_size" {
-  type        = number
-  description = "minimum size of the autoscaling group"
-  default     = 1
-}
-
-variable "launch_template_description" {
-  type        = string
-  description = "description of the launch template"
-  default     = ""
-}
-
-variable "iam_role_description" {
-  type        = string
-  description = "the description for the iam role to be created"
-  default     = ""
-}
-
-variable "avail_zones" {
-  type        = list(string)
-  description = "the list of availability zones to create subnets in"
-  default     = ["us-east-1a", "us-east-1b"]
 }
