fd65ccda2b3c8a1fdc78a79a54ff34be6e57f466
Andy Stoerkel <astoer1@students.towson.edu>
2022-09-23 08:03:11 -0400

    added multi-ec2 example

--- examples/multi-ec2/main.tf
+++ examples/multi-ec2/main.tf
@@ -7,16 +7,16 @@ module "autoscaling" {
   version = "6.5.2"
 
   for_each = {
-      one = {
-          name = "one"
-      }
-      two = {
-          name = "two"
-      }
+    one = {
+      instance_type = "t2.micro"
+    }
+    two = {
+      instance_type = "t2.large"
+    }
   }
 
-  name                = "example-asg"
-  vpc_zone_identifier = path.module.vpc.public_subnets
+  name                = "example-${each.key}"
+  vpc_zone_identifier = module.vpc.public_subnets
   min_size            = 1
   max_size            = 1
 
@@ -26,11 +26,11 @@ module "autoscaling" {
   launch_template_description = "Launch template example"
   update_default_version      = true
   image_id                    = "ami-06e07b42f153830d8"
-  instance_type               = "t2.micro"
+  instance_type               = each.value.instance_type
   user_data                   = base64encode(templatefile("${path.module}/containerAgent.sh", { CLUSTER_NAME = "example-ecs-ec2" })) # abstract name to vars, can't reference ecs module, cyclical dependency
 
 
-  security_groups = [module.security_group.security_group_id]
+  security_groups = [var.security_group_ids]
 
   # iam role creation
   create_iam_instance_profile = true
@@ -46,4 +46,28 @@ module "autoscaling" {
   }
 
   protect_from_scale_in = true
+}
+
+#################################
+#################################
+# Supporting resources
+#################################
+#################################
+
+
+#################################
+# vpc
+# https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
+#################################
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = "example-vpc"
+  cidr = "10.99.0.0/18"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.99.0.0/24", "10.99.1.0/24"]
+  private_subnets = ["10.99.3.0/24", "10.99.4.0/24"]
+
 }
