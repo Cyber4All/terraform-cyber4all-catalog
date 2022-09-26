ceb8793fa201d8dadae51a0763d2d5fe76f2891f
Andy Stoerkel <astoer1@students.towson.edu>
2022-09-26 10:36:08 -0400

    combined to a single module

--- examples/ec2/ec2-readme.md
+++ examples/ec2/ec2-readme.md
@@ -1,2 +1,7 @@
-# creating an EC2 instance using terraform-aws-modules/ec2-instance/aws
+# Creating an EC2 instance using terraform-aws-modules/ec2-instance/aws
 
+An EC2 instance will not show up in the Cluster if there is no way to connect to ECS endpoints, practically speaking this means ensuring that a nat gateway is created in the VPC:
+```
+create_nat_gateway = true
+single_nat_gateway = true
+```
--- modules/ecs-cluster/main.tf
+++ modules/ecs-cluster/main.tf
@@ -12,7 +12,83 @@ module "ecs" {
 
   autoscaling_capacity_providers = {
     one = {
-      auto_scaling_group_arn = var.autoscaling_group_arn
+      auto_scaling_group_arn = module.autoscaling.autoscaling_group_arn
+
+      managed_termination_protection = "ENABLED"
+
+      managed_scaling = {
+        maximum_scaling_step_size = 5
+        minimum_scaling_step_size = 1
+        status                    = "ENABLED"
+        target_capacity           = 60
+      }
+
+      default_capacity_provider_strategy = {
+        weight = 60
+        base   = 20
+      }
     }
   }
+}
+
+##################################
+# Required resources
+##################################
+
+
+#################################
+# security group
+# https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest
+#################################
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = "${var.project_name}-sg"
+  description = var.security_group_description
+  vpc_id      = var.vpc_id
+
+  ingress_with_cidr_blocks = var.ingress_with_cidr_blocks
+  egress_with_cidr_blocks  = var.egress_with_cidr_blocks
+}
+
+#################################
+# Auto-Scaling Group
+# https://registry.terraform.io/modules/terraform-aws-modules/autoscaling/aws/latest
+#################################
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name                = "${var.project_name}-asg"
+  vpc_zone_identifier = concat(var.public_subnets, var.private_subnets)
+  min_size            = var.asg_min_size
+  max_size            = var.asg_max_size
+
+  # launch template
+  create_launch_template      = true
+  launch_template_name        = "${var.project_name}-launch-template"
+  launch_template_description = var.launch_template_description
+  update_default_version      = true
+  image_id                    = var.launch_template_ami
+  instance_type               = var.instance_type
+  user_data                   = base64encode(templatefile("${path.module}/containerAgent.sh", { CLUSTER_NAME = "${var.project_name}-cluster" }))
+
+
+  security_groups = [module.security_group.security_group_id]
+
+  # iam role creation
+  create_iam_instance_profile = true
+  iam_role_name               = "${var.project_name}-iam-role-profile"
+  iam_role_description        = var.iam_role_description
+  iam_role_policies = {
+    AmazonEC2ContainerServiceforEC2Role = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+    AmazonSSMManagedInstanceCore        = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+  autoscaling_group_tags = {
+    AmazonECSManaged = true
+  }
+
+  protect_from_scale_in = true
 }
