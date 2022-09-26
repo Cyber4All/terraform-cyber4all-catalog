f457528dee9c7e8317bce3f18a8ad5e57706593a
Andy Stoerkel <astoer1@students.towson.edu>
2022-09-26 10:44:53 -0400

    removed locals

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -18,28 +18,7 @@ terraform {
 }
 
 locals {
-  project_name        = "example"
-  launch_template_ami = "ami-06e07b42f153830d8"
-  instance_type       = "t2.micro"
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 8080
-      to_port     = 8090
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "10.10.0.0/16"
-    },
-    {
-      rule        = "postgresql-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    }
-  ]
-  egress_with_cidr_blocks    = []
-  vpc_cidr                   = "10.99.0.0/18"
-  public_subnets             = ["10.99.0.0/24", "10.99.1.0/24"]
-  private_subnets            = ["10.99.3.0/24", "10.99.4.0/24"]
-  asg_max_size               = 1
-  security_group_description = "example security group"
+  project_name = "example"
 }
 
 #################################
@@ -50,11 +29,11 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "${local.project_name}-vpc"
-  cidr = local.vpc_cidr
+  cidr = "10.99.0.0/18"
 
   azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = local.public_subnets
-  private_subnets = local.private_subnets
+  public_subnets  = ["10.99.0.0/24", "10.99.1.0/24"]
+  private_subnets = ["10.99.3.0/24", "10.99.4.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
@@ -71,14 +50,26 @@ module "ecs-cluster" {
   vpc_id = module.vpc.vpc_id
 
   # allow ssh from anywhere
-  ingress_with_cidr_blocks = local.ingress_with_cidr_blocks
-  egress_with_cidr_blocks  = local.egress_with_cidr_blocks
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  egress_with_cidr_blocks  = []
 
   private_subnets = module.vpc.private_subnets
   public_subnets  = module.vpc.public_subnets
-  asg_max_size    = local.asg_max_size
+  asg_max_size    = 1
 
   # launch template
-  launch_template_ami = local.launch_template_ami
-  instance_type       = local.instance_type
+  launch_template_ami = "ami-06e07b42f153830d8"
+  instance_type       = "t2.micro"
 }
