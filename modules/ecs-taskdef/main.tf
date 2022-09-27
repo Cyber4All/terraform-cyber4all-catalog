2ce50f0481dabd74a3ed1ec0339d74eb5f2c2de8
Paulo Manalo <pmanal1@students.towson.edu>
2022-09-27 14:31:43 -0400

    Adds docs and all possible input variables

--- modules/ecs-taskdef/main.tf
+++ modules/ecs-taskdef/main.tf
@@ -17,9 +17,12 @@ terraform {
   }
 }
 
+# Creates an ECS task definition in AWS
+# Terraform Docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_task_definition
+# AWS Docs: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html
 resource "aws_ecs_task_definition" "example" {
-  family                   = "example"
+  family                   = var.family
   container_definitions    = file("task-definitions/example1.json")
-  requires_compatibilities = ["EC2"]
-  network_mode             = "awsvpc"
+  requires_compatibilities = var.requires_compatibilities
+  network_mode             = var.network_mode
 }
