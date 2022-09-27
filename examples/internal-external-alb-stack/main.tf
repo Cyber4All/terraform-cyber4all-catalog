5e05f7611d67ae99b07b39b44cb5ec553ff0cb12
Cwagne17 <cwagne17@students.towson.edu>
2022-09-27 10:33:19 -0400

    add default vars in example

--- examples/internal-external-alb-stack/main.tf
+++ examples/internal-external-alb-stack/main.tf
@@ -15,38 +15,38 @@ module "vpc" {
 module "alb-stack" {
   source = "../../modules/alb"
 
-  name = "alb-stack-example"
-  region                                            = "us-east-1"
+  name   = "alb-stack-example"
+  region = "us-east-1"
 
   # Network config
-  vpc_id = module.vpc.vpc_id
-  vpc_cidr                                          = module.vpc.vpc_cidr_block
+  vpc_id              = module.vpc.vpc_id
+  vpc_cidr            = module.vpc.vpc_cidr_block
   private_subnet_arns = module.vpc.private_subnet_arns
-  public_subnet_arns = module.vpc.public_subnet_arns
+  public_subnet_arns  = module.vpc.public_subnet_arns
 
   # Using default Security Groups
 
   # External ALB config
-  external_target_groups                            = []
+  external_target_groups = []
 
   external_http_tcp_listeners      = []
   external_http_tcp_listener_rules = []
 
-  external_https_listeners         = []
-  external_https_listener_rules    = []
+  external_https_listeners      = []
+  external_https_listener_rules = []
 
-  external_access_logs             = {}
+  external_access_logs = {}
 
   # Internal ALB config
-  internal_target_groups                            = []
+  internal_target_groups = []
 
-  internal_http_tcp_listeners                       = []
-  internal_http_tcp_listener_rules                  = []
+  internal_http_tcp_listeners      = []
+  internal_http_tcp_listener_rules = []
 
-  internal_https_listeners                          = []
-  internal_https_listener_rules                     = []
+  internal_https_listeners      = []
+  internal_https_listener_rules = []
 
-  internal_access_logs                              = {}
+  internal_access_logs = {}
 }
 
 module "internal-alb" {
@@ -57,12 +57,12 @@ module "internal-alb" {
 
   name = "example-internal-alb"
 
-  load_balancer_type = "application"
-  internal = true
+  load_balancer_type               = "application"
+  internal                         = true
   enable_cross_zone_load_balancing = true
 
-  vpc_id = module.vpc.vpc_id
-  subnets = module.vpc.private_subnet_arns
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.private_subnet_arns
   security_groups = [module.sg.security_group_id]
 
   # listeners
@@ -71,67 +71,67 @@ module "internal-alb" {
       target_group_index = 0
 
       protocol = "HTTP"
-      port = 80
+      port     = 80
     }
   ]
 
   http_tcp_listener_rules = [
     {
-        http_tcp_listener_index = 0
+      http_tcp_listener_index = 0
 
-        actions = [{
-            type = "forward"
-            target_group_index = 0
-        }]
-        conditions = [{
-            path_patterns = ["/service1"]
-        }]
+      actions = [{
+        type               = "forward"
+        target_group_index = 0
+      }]
+      conditions = [{
+        path_patterns = ["/service1"]
+      }]
     },
     {
-        http_tcp_listener_index = 0
+      http_tcp_listener_index = 0
 
-        actions = [{
-            type = "forward"
-            target_group_index = 1
-        }]
-        conditions = [{
-            path_patterns = ["/service2"]
-        }]
+      actions = [{
+        type               = "forward"
+        target_group_index = 1
+      }]
+      conditions = [{
+        path_patterns = ["/service2"]
+      }]
     }
   ]
 
   # targets
 
-  target_groups =  [
+  target_groups = [
     {
-        name = "Service 1 Target Group"
-        
-        backend_protocol = 80
-        backend_protocol = "HTTP"
-        protocol_version = "HTTP2"
-        target_type = "instance"
+      name = "Service 1 Target Group"
 
-        # deregistration_delay = 300 (default) should be longer then StopTimeout in task-defintion and Client Connection Timeout
+      backend_protocol = 80
+      backend_protocol = "HTTP"
+      protocol_version = "HTTP2"
+      target_type      = "instance"
 
-        health_check = {
-            matcher = "200-299"
-            path = "/"
-        }
+      # deregistration_delay = 300 (default) should be longer then StopTimeout in task-defintion and Client Connection Timeout
+
+      health_check = {
+        matcher = "200-299"
+        path    = "/"
+      }
     },
     {
-        name = "Service 2 Target Group"
-        
-        backend_protocol = 80
-        backend_protocol = "HTTP"
-        protocol_version = "HTTP2"
-        target_type = "instance"
+      name = "Service 2 Target Group"
 
-        # deregistration_delay = 300 (default) should be longer then StopTimeout in task-defintion and Client Connection Timeout
+      backend_protocol = 80
+      backend_protocol = "HTTP"
+      protocol_version = "HTTP2"
+      target_type      = "instance"
 
-        health_check = {
-            matcher = "200-299"
-            path = "/"
-        }
+      # deregistration_delay = 300 (default) should be longer then StopTimeout in task-defintion and Client Connection Timeout
+
+      health_check = {
+        matcher = "200-299"
+        path    = "/"
+      }
     }
-  ] 
+  ]
 }
