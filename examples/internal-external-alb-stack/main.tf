1c386c30866533a299787c13946722a07248602b
Cwagne17 <cwagne17@students.towson.edu>
2022-09-27 10:19:03 -0400

    add readme.md

--- examples/internal-external-alb-stack/main.tf
+++ examples/internal-external-alb-stack/main.tf
@@ -12,17 +12,10 @@ module "vpc" {
   create_egress_only_igw = true
 }
 
-module "sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 4.0"
+module "alb-stack" {
+  source = "../../modules/alb"
 
-  name        = "alb-sg"
-  description = "Security group for example usage with ALB"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "all-icmp"]
-  egress_rules        = ["all-all"]
+  
 }
 
 module "internal-alb" {
