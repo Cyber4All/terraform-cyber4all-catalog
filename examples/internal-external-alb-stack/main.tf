0666bf32f2f882d052b4897d5338fe5131283e70
Cwagne17 <cwagne17@students.towson.edu>
2022-09-27 12:29:11 -0400

    add example, untested

--- examples/internal-external-alb-stack/main.tf
+++ examples/internal-external-alb-stack/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_version = "1.2.9"
+
+  backend "s3" {
+    bucket = "competency-service-terraform-state"
+    key    = "live/example/internal-external-alb-stack/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "competency-service-terraform-locks"
+    encrypt        = true
+  }
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.4"
@@ -12,6 +25,12 @@ module "vpc" {
   create_egress_only_igw = true
 }
 
+# still requires testing with ecs services, acm, and s3 logging
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "example-log-bucket"
+}
+
 module "alb-stack" {
   source = "../../modules/alb"
 
@@ -19,7 +38,7 @@ module "alb-stack" {
   region = "us-east-1"
 
   # Network config
-  vpc_id              =    module.vpc.vpc_id
+  vpc_id              = module.vpc.vpc_id
   vpc_cidr            = module.vpc.vpc_cidr_block
   private_subnet_arns = module.vpc.private_subnet_arns
   public_subnet_arns  = module.vpc.public_subnet_arns
@@ -27,61 +46,55 @@ module "alb-stack" {
   # Using default Security Groups
 
   # External ALB config
-  external_target_groups = []
-
-  external_http_tcp_listeners      = []
+  external_http_tcp_listeners = [
+    { # permanently redirect HTTP to HTTPS
+      port        = 80
+      action_type = "redirect"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_codr = "HTTP_301"
+      }
+    }
+  ]
   external_http_tcp_listener_rules = []
 
-  external_https_listeners      = []
+  external_https_listeners = [
+    {
+      port               = 443
+      protocol           = "HTTPS"
+      certificate_arn    = "" # needs cert
+      target_group_index = 0  # should fwd to api-gateway
+    }
+  ]
   external_https_listener_rules = []
 
-  external_access_logs = {}
-
-  # Internal ALB config
-  internal_target_groups = []
-
-  internal_http_tcp_listeners      = []
-  internal_http_tcp_listener_rules = []
-
-  internal_https_listeners      = []
-  internal_https_listener_rules = []
-
-  internal_access_logs = {}
-}
-
-module "internal-alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "8.1.0"
-
-  create_lb = true
-
-  name = "example-internal-alb"
-
-  load_balancer_type               = "application"
-  internal                         = true
-  enable_cross_zone_load_balancing = true
-
-  vpc_id          = module.vpc.vpc_id
-  subnets         = module.vpc.private_subnet_arns
-  security_groups = [module.sg.security_group_id]
-
-  # listeners
-  http_tcp_listeners = [
-    {
-      target_group_index = 0
-
-      protocol = "HTTP"
-      port     = 80
+  external_target_groups = [
+    { # instance target (ECS service will register target group association)
+      name             = "Api-Gateway"
+      backend_port     = 80
+      backend_protocol = "HTTP"
+      protocol_version = "HTTP2"
+      health_check = {
+        matcher = "200-299"
+        path    = "/"
+      }
     }
   ]
 
-  http_tcp_listener_rules = [
+  # Internal ALB config
+  internal_http_tcp_listeners = [
+    { # Internal ALB is referenced from service in public subnets (i.e API-Gateway)
+      port = 80
+    }
+  ]
+
+  internal_http_tcp_listener_rules = [
     {
       http_tcp_listener_index = 0
-
       actions = [{
         type               = "forward"
-        target_group_index = 0
+        target_group_index = 0 # micro-service 1
       }]
       conditions = [{
         path_patterns = ["/service1"]
@@ -89,10 +102,9 @@ module "internal-alb" {
     },
     {
       http_tcp_listener_index = 0
-
       actions = [{
         type               = "forward"
-        target_group_index = 1
+        target_group_index = 1 # micro-service 2
       }]
       conditions = [{
         path_patterns = ["/service2"]
@@ -100,38 +112,29 @@ module "internal-alb" {
     }
   ]
 
-  # targets
-
-  target_groups = [
+  internal_target_groups = [
     {
-      name = "Service 1 Target Group"
-
-      backend_protocol = 80
+      name             = "Service 1 Target Group"
+      backend_port     = 80
       backend_protocol = "HTTP"
       protocol_version = "HTTP2"
-      target_type      = "instance"
-
-      # deregistration_delay = 300 (default) should be longer then StopTimeout in task-defintion and Client Connection Timeout
-
       health_check = {
         matcher = "200-299"
         path    = "/"
       }
     },
     {
-      name = "Service 2 Target Group"
-
-      backend_protocol = 80
+      name             = "Service 2 Target Group"
+      backend_port     = 80
       backend_protocol = "HTTP"
       protocol_version = "HTTP2"
       target_type      = "instance"
-
-      # deregistration_delay = 300 (default) should be longer then StopTimeout in task-defintion and Client Connection Timeout
-
       health_check = {
         matcher = "200-299"
         path    = "/"
       }
     }
   ]
+
+  access_log_bucket = "example-log-bucket"
 }
