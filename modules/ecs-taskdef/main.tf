990d4aa60e131c31218c534ae27e992c91eaf365
Paulo Manalo <pmanal1@students.towson.edu>
2022-09-27 10:34:50 -0400

    Adds json and resource to create ecs task def

---
+++ modules/ecs-taskdef/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "1.2.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.29.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "competency-service-terraform-state"
+    key    = "live/example/ecs/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "competency-service-terraform-locks"
+    encrypt        = true
+  }
+}
+
+resource "aws_ecs_task_definition" "example" {
+  family = "example"
+  container_definitions = file("task-definitions/example1.json")
+  requires_compatibilities = [ "EC2" ]
+  network_mode = "awsvpc"
+}
