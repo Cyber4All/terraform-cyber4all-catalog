335c1f7f2989406b251062699f8670a4f8d2b61a
Paulo Manalo <pmanal1@students.towson.edu>
2022-10-04 14:35:40 -0400

    Linting

--- modules/ecs-cluster/outputs.tf
+++ modules/ecs-cluster/outputs.tf
@@ -8,6 +8,11 @@ output "cluster_id" {
   value       = module.ecs.cluster_name
 }
 
+output "cluster_arn" {
+  description = "the arn of the ECS cluster"
+  value       = module.ecs.cluster_arn
+}
+
 output "autoscaling_group_id" {
   description = "the id of the generated autoscaling group"
   value       = module.autoscaling.autoscaling_group_id
