6f7c3d716b44936a64bd1fee0854a932ec2b730a
Cwagne17 <cwagne17@students.towson.edu>
2022-10-20 11:32:46 -0400

    reformatted ecs-service module

--- modules/ecs-service/main.tf
+++ modules/ecs-service/main.tf
@@ -1,40 +1,116 @@
-# Creates an ECS task definition in AWS
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE ECS TASK DEFINITION
+#
 # Terraform Docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_task_definition
 # AWS Docs: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html
-resource "aws_ecs_task_definition" "example" {
-  family                   = var.ecs_taskdef_family
-  container_definitions    = var.ecs_taskdef_container_definitions
-  requires_compatibilities = var.requires_compatibilities
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_ecs_task_definition" "task" {
+  family                   = var.task_family
+  container_definitions    = var.container_definitions
+  
   network_mode             = var.network_mode
+  requires_compatibilities = ["EC2", "FARGATE"]
 
-  # Defaults
-  # network_mode = "default"
-  # tags = {}
-  # ipc_mode = "shareable"
-}
-
-# Creates an ECS service in AWS
-# Terraform Docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service
-# AWS Docs: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/service_definition_parameters.html
-resource "aws_ecs_service" "example" {
-  name            = var.ecs_service_name
-  task_definition = aws_ecs_task_definition.example.arn
-  cluster         = var.ecs_service_cluster_arn
-  desired_count   = var.ecs_service_num_tasks
-
-  network_configuration {
-    subnets         = var.ecs_service_subnets
-    security_groups = [var.ecs_service_security_group_id]
+  runtime_platform {
+    operating_system_family = var.operating_system_family
+    cpu_architecture = var.cpu_architecture
   }
 
-  # Defaults
-  # cluster = (assumes default cluster)
-  # deployment_controller = "ECS"
-  # deployment_minimum_healthy_percent = 100
-  # desired_count = 0
-  # enable_ecs_managed_tags = false
-  # health_check_grace_period_seconds = 0
-  # launch_type = "EC2"
-  # scheduling_strategy = "REPLICA"
-  # wait_for_steady_state = false
+  # ----------------------------------------------------
+  # HARDWARE SIZES
+  # ----------------------------------------------------
+  cpu = var.task_cpu
+  ephemeral_storage {
+    size_in_gib = var.task_ephemeral_storage
+  }
+  memory = var.task_memory
+
+  # ----------------------------------------------------
+  # ECS TASK AUTH
+  # ----------------------------------------------------
+  task_role_arn = var.task_role_arn
+  execution_role_arn = var.execution_role_arn
+
+  # ----------------------------------------------------
+  # DEFAULTS
+  # ----------------------------------------------------
+
+  /* inference_accelerator {} */
+  /* ipc_mode = "shareable" */
+  /* pid_mode = host | task */
+  /* placement_constraints {} */
+  /* proxy_configuration {} */
+  /* skip_destroy = false */
+  /* tags = {} */
+  /* volume {} */
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE ECS SERVICE
+#
+# Terraform Docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service
+# AWS Docs: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/service_definition_parameters.html
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_ecs_service" "service" {
+  name            = var.service_name
+
+  cluster         = var.cluster_arn
+  task_definition = aws_ecs_task_definition.task.arn
+
+  launch_type = var.launch_type
+  enable_ecs_managed_tags = true
+
+  # ----------------------------------------------------
+  # DEPLOYMENT CONFIG
+  # ----------------------------------------------------
+  desired_count   = var.desired_count
+  enable_execute_command = false
+  force_new_deployment = true
+  health_check_grace_period_seconds = 60
+
+  # ----------------------------------------------------
+  # LOAD BALANCER CONFIG
+  # ----------------------------------------------------
+  load_balancer {
+    target_group_arn = var.target_group_arn
+    container_name = var.container_name
+    container_port = var.container_port
+  }
+
+  # ----------------------------------------------------
+  # NETWORK CONFIG
+  # ----------------------------------------------------
+  network_configuration {
+    subnets         = var.service_subnets
+    security_groups = [var.service_security_group_id]
+    assign_public_ip = var.assign_public_ip # can be true for FARGATE
+  }
+
+  # ----------------------------------------------------
+  # TASK PLACEMENT
+  # ----------------------------------------------------
+  ordered_placement_strategy {
+    type = "spread"
+    field = "instanceId"
+  }
+
+  # ----------------------------------------------------
+  # DEFAULTS
+  # ----------------------------------------------------
+ 
+  /* capacity_provider_strategy {} */
+  /* deployment_circuit_breaker {} */
+  /* deployment_controller {} */
+  /* deployment_maximum_percent = 200 */
+  /* deployment_minimum_healthy_percent = 100 */
+  /* iam_role = null */
+  /* placement_constraints {} */
+  /* platform_version = "LATEST" */
+  /* propagate_tags = null */
+  /* scheduling_strategy = "REPLICA" */
+  /* service_registries {} */
+  /* tags = {} */
+  /* wait_for_steady_state = false */
 }
