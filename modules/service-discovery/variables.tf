ec1827127a9172adb882b2c8c3b2320c1cd7b75f
Cwagne17 <cwagne17@students.towson.edu>
2022-10-25 14:00:34 -0400

    add service-discovery module

--- modules/service-discovery/variables.tf
+++ modules/service-discovery/variables.tf
@@ -1,12 +1,25 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
 variable "name" {
   type = string
-}
-
-variable "description" {
-  type    = string
-  default = ""
+  description = "The name of the namespace."
 }
 
 variable "vpc_id" {
   type = string
+  description = "The ID of VPC that you want to associate the namespace with."
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "description" {
+  type    = string
+  desdescription = "The description that you specify for the namespace when you create it."
+  default     = "Private DNS Namespace Managed by Terraform"
 }
