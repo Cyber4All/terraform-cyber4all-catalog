dbd32c22bbb40ca6a1a0ed56c0151d186d4fd425
Andy Stoerkel <astoer1@students.towson.edu>
2022-11-11 08:54:00 -0500

    added better block comments

--- modules/MongoCluster/example/mongoCluster/main.tf
+++ modules/MongoCluster/example/mongoCluster/main.tf
@@ -17,9 +17,9 @@ resource "mongodbatlas_cluster" "cluster-test" {
   name       = "cluster-test-global"
 
   # Provider Settings "block"
-  provider_name         = "TENANT"
-  backing_provider_name = "AWS"
-  provider_region_name  = "US_EAST_1"
+  provider_name               = "TENANT"
+  backing_provider_name       = "AWS"
+  provider_region_name        = "US_EAST_1"
   provider_instance_size_name = "M2"
 }
 
