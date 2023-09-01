4e82d41caf455ab99b6f0f8f0c69c8ab7beadbc0
Cwagne17 <cwagne17@students.towson.edu>
2023-09-01 09:28:12 -0400

    Comment out integration tests since none have been added yet

--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -2,7 +2,6 @@
 # AWS S3 BUCKET
 # ---------------------------------------------------------------------------------------------------------------------
 
-# tfsec:ignore:aws-s3-enable-bucket-logging
 
 resource "aws_s3_bucket" "backend" {
   bucket = var.bucket_name
