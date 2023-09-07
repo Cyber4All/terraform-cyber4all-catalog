32a687f9359233e8c09c4dab90e3503864827974
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2023-09-07 13:58:23 -0400

    added public access block per tflinter thingy

--- modules/s3-articfact/main.tf
+++ modules/s3-articfact/main.tf
@@ -112,6 +112,15 @@ resource "aws_s3_bucket_lifecycle_configuration" "s3_artifact_full_lifecycle_man
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "s3_artifact" {
+  bucket = aws_s3_bucket.s3_artifact.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 # -------------------------------------------
 # CREATE S3 REPLICATION CONFIGURATION
 # -------------------------------------------
