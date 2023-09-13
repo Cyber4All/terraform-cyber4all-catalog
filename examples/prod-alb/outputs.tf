470646e0e41983bd38a44001edb746ed670eacc2
Cwagne17 <cwagne17@students.towson.edu>
2023-09-13 13:43:57 -0400

    update outputs for prod example

--- examples/prod-alb/outputs.tf
+++ examples/prod-alb/outputs.tf
@@ -8,9 +8,19 @@ output "alb_dns_name" {
   value       = module.alb.alb_dns_name
 }
 
+output "alb_hosted_zone_id" {
+  description = "The ID of the hosted zone where the ALB DNS record was created."
+  value       = module.alb.alb_hosted_zone_id
+}
+
 output "alb_name" {
   description = "The name of the ALB."
   value       = module.alb.alb_name
+}
+
+output "alb_dns_record_name" {
+  description = "The name of the ALB DNS record."
+  value       = module.alb.alb_dns_record_name
 }
 
 output "alb_security_group_id" {
@@ -21,4 +31,9 @@ output "alb_security_group_id" {
 output "http_listener_arn" {
   description = "The ARN of the HTTP listener."
   value       = module.alb.http_listener_arn
+}
+
+output "https_listener_arn" {
+  description = "The ARN of the HTTPS listener."
+  value       = module.alb.https_listener_arn
 }
