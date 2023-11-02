36ae80d5d6863f2a001fdefdaa53d916d9ceac38
chris wagner <cwagne17@students.towson.edu>
2023-11-02 16:07:23 -0400

    add dependencies directory in the examples

---
+++ examples/dependencies/deploy-vpc-only/main.tf
@@ -0,0 +1,25 @@
+# ------------------------------------------------------------------------------
+# DEPLOY AN AWS VPC
+#
+# This example shows how to deploy a single VPC in a given region. This example
+# will create public and private subnets in each availability zone in the region.
+# ------------------------------------------------------------------------------
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  vpc_name = "vpc-${var.random_id}"
+}
