18cffa05f5a30c01e0ffd9da810f37f7199add6b
Christohper Wagner <cwagne17@christophers-macbook-pro.towson.edu>
2023-11-06 10:32:43 -0500

    remove unused variable region from admin example spacelift

--- examples/deploy-spacelift-admin-stack/variables.tf
+++ examples/deploy-spacelift-admin-stack/variables.tf
@@ -17,12 +17,6 @@
 
 # --------------------------------------------------------------------
 
-variable "region" {
-  description = "The AWS region to provision resources to."
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "random_id" {
   description = "Random id generated for the purpose of testing"
   type        = string
