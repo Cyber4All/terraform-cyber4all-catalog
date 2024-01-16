35758ce1c0b465b58bddc203ca1302de981be6c3
Christohper Wagner <cwagne17@students.towson.edu>
2024-01-16 15:09:03 -0500

    add all test suites back

--- test/terraform_module_test.go
+++ test/terraform_module_test.go
@@ -36,22 +36,22 @@ func TestExamplesForTerraformModules(t *testing.T) {
 	 */
 	tests := [][]TestCase{
 		{
-			// {
-			// 	name:            "spacelift-admin",
-			// 	workingDir:      "../examples/deploy-spacelift-admin-stack",
-			// 	genTestDataFunc: modules.DeploySpaceliftAdminStack,
-			// 	validateFunc:    modules.ValidateSpaceliftAdminStack,
-			// },
+			{
+				name:            "spacelift-admin",
+				workingDir:      "../examples/deploy-spacelift-admin-stack",
+				genTestDataFunc: modules.DeploySpaceliftAdminStack,
+				validateFunc:    modules.ValidateSpaceliftAdminStack,
+			},
 
 			// Only run this test locally as it may conflict in CI
 			// mongodb-cluster: Deploy and validate a MongoDB cluster. (~686.96s)
 			// This test requires a VPC.
-			// {
-			// 	name:            "mongodb-cluster",
-			// 	workingDir:      "../examples/deploy-mongodb-cluster",
-			// 	genTestDataFunc: modules.DeployMongoDBCluster,
-			// 	validateFunc:    modules.ValidateMongoDBCluster,
-			// },
+			{
+				name:            "mongodb-cluster",
+				workingDir:      "../examples/deploy-mongodb-cluster",
+				genTestDataFunc: modules.DeployMongoDBCluster,
+				validateFunc:    modules.ValidateMongoDBCluster,
+			},
 
 			// ecs_service: Deploy and validate an ECS service. (~912s)
 			// This test requires a VPC.
@@ -64,30 +64,30 @@ func TestExamplesForTerraformModules(t *testing.T) {
 
 			// ecs-cluster: Deploy and validate an ECS cluster. (~313s)
 			// This test requires a VPC.
-			// {
-			// 	name:            "ecs-cluster",
-			// 	workingDir:      "../examples/deploy-ecs-cluster",
-			// 	genTestDataFunc: modules.DeployEcsClusterUsingTerraform,
-			// 	validateFunc:    modules.ValidateEcsCluster,
-			// },
+			{
+				name:            "ecs-cluster",
+				workingDir:      "../examples/deploy-ecs-cluster",
+				genTestDataFunc: modules.DeployEcsClusterUsingTerraform,
+				validateFunc:    modules.ValidateEcsCluster,
+			},
 
 			// alb-https: Deploy and validate an Application Load Balancer with HTTPS. (~268s)
 			// This test requires a VPC.
-			// {
-			// 	name:            "alb",
-			// 	workingDir:      "../examples/deploy-alb",
-			// 	genTestDataFunc: modules.DeployAlb,
-			// 	validateFunc:    modules.ValidateAlbHttps,
-			// },
+			{
+				name:            "alb",
+				workingDir:      "../examples/deploy-alb",
+				genTestDataFunc: modules.DeployAlb,
+				validateFunc:    modules.ValidateAlbHttps,
+			},
 
 			// vpc: Deploy and validate a VPC. (~100s)
 			// This test requires a VPC.
-			// {
-			// 	name:            "vpc",
-			// 	workingDir:      "../examples/deploy-vpc",
-			// 	genTestDataFunc: modules.DeployVpcUsingTerraform,
-			// 	validateFunc:    modules.ValidateVpc,
-			// },
+			{
+				name:            "vpc",
+				workingDir:      "../examples/deploy-vpc",
+				genTestDataFunc: modules.DeployVpcUsingTerraform,
+				validateFunc:    modules.ValidateVpc,
+			},
 		},
 	}
 
