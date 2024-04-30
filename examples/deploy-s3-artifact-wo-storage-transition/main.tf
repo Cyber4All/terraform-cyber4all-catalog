31f3453fcbf1a5d05a36fdc4f20f0fc4437d6298
Michael Franz <michael.franz@secured.team>
2024-04-30 10:48:28 -0400

    forgot to add new files

---
+++ examples/deploy-s3-artifact-wo-storage-transition/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+module "s3_artifact" {
+  source = "../../modules/s3-artifact"
+
+  bucket_name                 = var.bucket_name
+}
